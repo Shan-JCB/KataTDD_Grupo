d0eb409648cb267d52e26e17779c14fca79b59cc
Shan-JCB <71997263@continental.edu.pe>
2024-06-02 23:37:51 -0500

    Caso 2: Calcular promedio con un conjunto (datos/peso) de un elemento (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -7,3 +7,9 @@ class Conjunto:
         if not self.__datos or not self.__pesos or len(self.__datos) != len(self.__pesos):
             return None
 
+        if len(self.__datos) == 1:  # Caso de un solo elemento
+            return self.__datos[0]
+
+        suma_ponderada = sum(d * p for d, p in zip(self.__datos, self.__pesos))
+        suma_pesos = sum(self.__pesos)
+        return suma_ponderada / suma_pesos
