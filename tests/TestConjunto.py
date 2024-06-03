7dabb1c309a8346c08ed9629e5e020d0884880fb
Shan-JCB <71997263@continental.edu.pe>
2024-06-02 23:33:06 -0500

    Caso 1: Calcular promedio con conjunto (datos/peso) vacío (Rojo)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,8 +1,15 @@
 import unittest
 from src.logica.Conjunto import Conjunto
 
-class TestConjunto( unittest.TestCase ):
-    pass
+class TestConjunto(unittest.TestCase):
+
+    def test_conjunto_vacio_retornaNone(self):
+        datos = []
+        pesos = []
+        conjunto = Conjunto(datos, pesos)
+        resultado = conjunto.promedio_ponderado()
+        self.assertIsNone(resultado)
+
 
 
 if __name__ == '__main__':
