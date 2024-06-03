c561c6b27e00c317a089907dc5fc3f0f539ef29c
Shan-JCB <71997263@continental.edu.pe>
2024-06-02 23:31:33 -0500

    Creando el esquelto de las clases Conjunto y TestConjunto

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,9 +1,8 @@
 import unittest
+from src.logica.Conjunto import Conjunto
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestConjunto( unittest.TestCase ):
+    pass
 
 
 if __name__ == '__main__':
